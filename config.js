cb9c3633e35b44e7e67c586c978075f463ed6f6f
greeceGit <greece_gls@163.com>
2017-05-16 18:53:15 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+var config={
+  mailto:'422704287@qq.com',
+  subject:'实习-北邮-郭丽斯',
+  html:"您好！<br>"+
+         "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我是北京邮电大学电子与通信工程专业研二的学生郭丽斯，在北邮人论坛看到贵部门招聘前端实习生的帖子,想要应聘该岗位。<br>"+
+        "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我在中国移动设计院有过为期一年的实习经历，担任前端后台实习生。实习期间开始学习前端相关知识，参与过多个项目的前端"+
+        "开发任务，项目主要利用bootstrap和jQuery完成。另外有过java后台开发经验，对前后端交互流程较为熟悉。项目之余也自学"+
+        "了原生JavaScript,HTML,CSS等前端基础知识，打下了比较扎实的前端基础知识。<br>"+
+      "  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;附上我的简历，请您查收。期待您的回复！电话：18810962203. <br>"+
+  "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;nbsp;"+
+  "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;nbsp;郭丽斯"
+};
+module.exports=config;
