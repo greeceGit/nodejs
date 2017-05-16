02601d2b9f8138ee0777c96addd3b8534f6cb880
greeceGit <greece_gls@163.com>
2017-05-16 18:57:16 +0800

    Update mail.js

--- mail.js
+++ mail.js
@@ -6,7 +6,7 @@ var config_email={
   port:25,
   auth: {
       user: 'greece_gls@163.com',
-      pass: 'asdfghj123',
+      pass: 'XXXXXXXXX',    //此处写授权码
   }
 };
 var transporter = nodemailer.createTransport(config_email);
@@ -20,8 +20,8 @@ var sendmail = function(who,subject,html){
     option.html= html;
     option.attachments= [
       {
-        filename : '前端-北邮-郭丽斯.pdf',
-        path: 'C:/Users/Greece/Desktop/求职相关/前端-北邮-郭丽斯.pdf'
+        filename : '前端-北邮-XXX.pdf',
+        path: 'C:/Users/Greece/Desktop/求职相关/前端-北邮-XXX.pdf'
       }
     ];
     transporter.sendMail(option, function(error, response){
@@ -34,10 +34,3 @@ var sendmail = function(who,subject,html){
 };
 //调用发送邮件
 sendmail(config.mailto,config.subject,config.html);
-// sendmail("422704287@qq.com","实习-北邮-郭丽斯","您好！<br>"+
-//        "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我是北京邮电大学电子与通信工程专业研二的学生郭丽斯，在北邮人论坛看到贵部门招聘前端实习生的帖子,想要应聘该岗位。<br>"+
-//       "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我在中国移动设计院有过为期一年的实习经历，担任前端后台实习生。实习期间开始学习前端相关知识，参与过多个项目的前端"+
-//       "开发任务，项目主要利用bootstrap和jQuery完成。另外有过java后台开发经验，对前后端交互流程较为熟悉。项目之余也自学"+
-//       "了原生JavaScript,HTML,CSS等前端基础知识，打下了比较扎实的前端基础知识。<br>"+
-//     "  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;附上我的简历，请您查收。期待您的回复！电话：18810962203. <br>"+
-// "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;郭丽斯");
